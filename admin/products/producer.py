9b284d9cb6276c0a9fb7900924671031103b059e
taiger-jpradass <josue.pradas@taiger.com>
2021-02-10 16:34:29 +0100

    added ack to rabbit

--- admin/products/producer.py
+++ admin/products/producer.py
@@ -1,4 +1,4 @@
-import pika
+import pika, json
 
 params = pika.URLParameters('amqp://guest:guest@admin_amqps_1:5672/host')
 
@@ -6,5 +6,6 @@ connection = pika.BlockingConnection(params)
 
 channel = connection.channel()
 
-def publish():
-    channel.basic_publish(exchange='', routing_key='admin', body='hello')
+def publish(method, body):
+    properties = pika.BasicProperties(method)
+    channel.basic_publish(exchange='', routing_key='main', body=json.dumps(body), properties=properties)
--- main/consumer.py
+++ main/consumer.py
@@ -1,4 +1,5 @@
-import pika
+import pika, json
+from main import Product, db
 
 params = pika.URLParameters('amqp://guest:guest@admin_amqps_1:5672/host')
 
@@ -10,7 +11,29 @@ channel.queue_declare(queue='main')
 
 def callback(ch, method, properties, body):
     print('Received in main')
-    print(body)
+    data = json.loads(body)
+    print(data)
+
+    if properties.content_type == 'product_created':
+        product = Product(id=data['id'], title=data['title'], image=data['image'])
+        db.session.add(product)
+        db.session.commit()
+        print('Product added')
+
+    elif properties.content_type == 'product_updated':
+        product = Product.query.get(data['id'])
+        product.title = data['title']
+        product.image = data['image']
+        db.session.commit()
+        print('Product updated')
+
+    elif properties.content_type == 'product_deleted':
+        product = Product.query.get(data)
+        db.session.delete(product)
+        db.session.commit()
+        print('Product deleted')
+
+    ch.basic_ack(delivery_tag=method.delivery_tag)
 
 channel.basic_consume(queue='main', on_message_callback=callback)
 
